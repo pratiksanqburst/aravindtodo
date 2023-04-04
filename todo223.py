00f6e36619099ef38167cd2f5756dd26f0c7ce95
pratiksanqburst <124756960+pratiksanqburst@users.noreply.github.com>
2023-04-04 10:19:45 +0530

    Update todo223.py

--- todo223.py
+++ todo223.py
@@ -1,8 +1,49 @@
-num1 = 1.5
-num2 = 6.3
+# define an empty list to hold our tasks
+tasks = []
 
-# Add two numbers
-sum = num1 + num2
+# define a function to add a task to the list
+def add_task(task):
+    # task = input("Enter task: ")
+    tasks.append(task)
+    print("Task added.")
+    return task
 
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# define a function to remove a task from the list
+def remove_task():
+    task = input("Enter task to remove: ")
+    if task in tasks:
+        tasks.remove(task)
+        print("Task removed.")
+        return task
+    else:
+        print("Task not found.")
+
+# define a function to display all tasks in the list
+def view_tasks():
+    if tasks:
+        for task in tasks:
+            print(task)
+            return tasks
+    else:
+        print("No tasks.")
+
+# define a main function to run the program
+def main():
+    while True:
+        print("\n1. Add task\n2. Remove task\n3. View tasks\n4. Quit")
+        choice = input("Enter choice: ")
+        if choice == "1":
+            add_task()
+        elif choice == "2":
+            remove_task()
+        elif choice == "3":
+            view_tasks()
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice.")
+
+if __name__ == "__main__":
+    main()
+
