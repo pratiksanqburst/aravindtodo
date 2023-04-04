d024e1082541cf1caf33d2067096bc1055b9b366
pratiksanqburst <124756960+pratiksanqburst@users.noreply.github.com>
2023-04-04 10:19:03 +0530

    Update todo21.py

--- todo21.py
+++ todo21.py
@@ -1,1 +1,25 @@
-print("This line will be printed.")
+import unittest
+import todoo as op
+
+class Testtodo(unittest.TestCase):
+
+    def setUp(self):
+        op.tasks = []
+
+    def test_add(self):
+        self.assertEqual(op.add_task('hello'),'hello')
+        self.assertEqual(op.add_task('work'),'work')
+
+    def test_view1(self):
+        op.add_task('hello')
+        op.add_task('work')
+        self.assertEqual(op.view_task(),['hello', 'work'])
+
+    def test_remove(self):
+        op.add_task('hello')
+        op.add_task('work')
+        self.assertEqual(op.remove_task(),'hello')
+        self.assertEqual(op.remove_task(),'work')
+
+    def test_view(self):
+        self.assertEqual(op.view_task(),['hello','work'])
